9bc02fe4257b788c771f2f222f003ce568e95599
Haffner32 <adenchan01@gmail.com>
2025-06-12 11:21:09 +0800

    Functionality for 1. Sending friend requests 2. Accepting friend requests 3. Viewing your current friends 4. Getting incoming friend requests

--- firebase.ts
+++ firebase.ts
@@ -14,8 +14,10 @@ import {
   addDoc,
   getDocs,
   query,
-  orderBy
-} from 'firebase/firestore';
+  orderBy,
+  doc,
+  setDoc,
+  deleteDoc} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDB0LQru_C36-S07Zfk0D470F1czxJ6XUg',
@@ -75,5 +77,70 @@ export async function getMealLogs(uid: string) {
   const colRef = collection(db, 'users', uid, 'mealLogs');
   const q      = query(colRef, orderBy('timestamp', 'desc'));
   const snap   = await getDocs(q);
+  return snap.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+}
+
+/**
+ *
+ * @param uid
+ * @param friendUid
+ * Sends a request from uid to friendUid
+ */
+export async function addFriend(uid: string, friendUid: string) {
+  const outgoingRef = doc(db, 'users', uid, 'outgoingRequests', friendUid);
+  const incomingRef = doc(db, 'users', friendUid, 'incomingRequests', uid);
+
+  const requestData = {
+    from: uid,
+    to: friendUid,
+    status: 'pending',
+    timestamp: Date.now(),
+  };
+
+  await setDoc(outgoingRef, requestData);
+  await setDoc(incomingRef, requestData);
+
+}
+
+/**
+ * Accept a friend request
+ */
+export async function acceptFriend(uid: string, friendUid: string) {
+  const incomingRef = doc(db, 'users', friendUid, 'incomingRequests', uid);
+  const outgoingRef = doc(db, 'users', uid, 'outgoingRequests', friendUid);
+
+  const myFriendRef = doc(db, 'users', uid, 'friends', friendUid);
+  const theirFriendRef = doc(db, 'users', friendUid, 'friends', uid);
+  //Add to friends for both users
+  await setDoc(myFriendRef, {
+    uid: friendUid,
+    since: Date.now(),
+  });
+  await setDoc(theirFriendRef, {
+    uid: uid,
+    since: Date.now(),
+  })
+  //Remove the friend requests
+  await deleteDoc(incomingRef);
+  await deleteDoc(outgoingRef);
+
+}
+
+/**
+ * Get list of current friends
+ */
+
+export async function getFriends(uid: string) {
+  const myFriendsRef = collection(db, 'users', uid, 'friends');
+  const snap = await getDocs(myFriendsRef);
+  return snap.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+}
+
+/**
+ * Get incoming friend requests
+ */
+export async function getIncomingRequests(uid: string) {
+  const incomingRef = collection(db, 'users', uid, 'incomingRequests');
+  const snap = await getDocs(incomingRef);
   return snap.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 }
